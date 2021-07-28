ca99243b516f02706f9e2ed32fd932324b578dbc
Ricardo Montenegro <ramn21@gmail.com>
2021-07-28 21:53:32 +0200

    Add validations for name lastname and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :lastname, :email
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX
 end
