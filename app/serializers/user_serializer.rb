df2fa6c12fa79e4ecbef6eed13b98a7f72859072
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:31:47 +0200

    Delete commented code

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,7 +4,6 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :lastname, :email, :job, :phone, :avatar
 
   def avatar
-    # Rails.application.routes.url_helpers.rails_representation_url(object.avatar, only_path: true)
     rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
   end
 end
