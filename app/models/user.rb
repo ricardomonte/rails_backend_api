521411f54a98d37e5a06996aca78ec8c43b297e7
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:29:57 +0200

    Add validation for job and phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  validates_presence_of :name, :lastname, :email
+  validates_presence_of :name, :lastname, :email, :job, :phone
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email, with: VALID_EMAIL_REGEX
