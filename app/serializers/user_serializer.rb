b7a77f8aa670127740d563ca6af3b19630db1776
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:31:19 +0200

    Add job, phone and avatar attributes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :lastname, :email
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :lastname, :email, :job, :phone, :avatar
+
+  def avatar
+    # Rails.application.routes.url_helpers.rails_representation_url(object.avatar, only_path: true)
+    rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
+  end
 end
