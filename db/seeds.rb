5fab313a3b938df6dfcbbc0fa962baf326af831e
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:33:31 +0200

    Add logic for seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+require 'uri'
 require 'faker'
 
 User.destroy_all
 
+arr_users = []
+
 10.times {
-  User.create(name: Faker::Name.first_name, lastname: Faker::Name.last_name, email: Faker::Internet.email)
+  user = User.create(
+    name: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    job: Faker::Job.title,
+    phone: Faker::PhoneNumber.cell_phone_in_e164
+  )
+  arr_users << user
 }
+
+arr_users.each do |user|
+  img = URI.open("https://robohash.org/#{user.name}.png")
+  user.avatar.attach(io: img, filename: "#{user.name}.png", content_type: 'image/png')
+end
+
+puts 'seed success'
