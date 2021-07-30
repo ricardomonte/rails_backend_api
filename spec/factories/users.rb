3f729efc00f115272f0965b037ef6fff0657a0c2
Ricardo Montenegro <ramn21@gmail.com>
2021-07-30 03:36:20 +0200

    Add job, phone and avatar in factory user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     name { "Adam" }
     lastname { "Doe" }
     email { "AdamDoe@doe.com" }
+    job { "Testing" }
+    phone { "+123456789" }
+    after(:build) do |user|
+      user.avatar.attach(io: File.open(Rails.root.join('spec', 'factories', 'image', 'logo192.png')), filename: 'logo192.png', content_type: 'image/png')
+    end
   end
 end
